54809ca0ccc28844eed29a969c18cd4c7f779ef8
ZacRayTho <zachary.thomas@student.awesomeinc.org>
2023-02-23 20:24:50 +0000

    mvp with using classes

--- main2.js
+++ main2.js
@@ -1,4 +1,5 @@
 //Model
+//the brain,holds the data
 class gameData {
     //every class has to have a constructor() method?? (according to w3schools)
     //constructor method automatically called when new object is created
@@ -8,13 +9,23 @@ class gameData {
         this.board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
         //evens turns will be Xs ,odds will be Os
         this.turnNum = 0
-
+        this.winBoard = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
     }
 
 
 }
 
 //View
+//what the user interacts with and sees 
 class gameView {
     constructor() {
         //grab only div available
@@ -22,11 +33,13 @@ class gameView {
 
     }
     init() {
+        this.app.innerHTML = "";
+        app.model.board.fill(0)
         //create row that I plan to append 9 col-4 to for tiles
         this.elementMake("div", "row", "", "", "", this.app, "")
         this.row = document.getElementsByClassName("row");
         for (let i = 0; i < 9; i++) {
-            let tile = this.elementMake("div", "col-4 border text-center d-flex align-items-center", "background-color: grey; min-height: 200px;", i, `app.clickMe(${i})`, this.row[0], "")
+            this.elementMake("div", "col-4 border text-center d-flex align-items-center", "background-color: grey; min-height: 200px;", i, `app.clickMe(${i})`, this.row[0], "")
         }
     }
 
@@ -44,34 +57,78 @@ class gameView {
 }
 
 //Controller/ the brains???
+//between model and view passing info back and forth
 //possible just the event on tile click? 9 gameControllers?
 
 class gameController {
     constructor(model, view) {
         this.model = model
         this.view = view
-
     }
 
     clickMe(index) {
         //evens Xs [0,2,4,6,8] / odds Os [1,3,5,7]
         let tiles = document.getElementsByClassName("col-4")
         if (this.model.turnNum % 2 == 0 && this.model.board[index] == 0) {
-            console.log("hellooooooo")
-            tiles[index].innerHTML = "x";
+            tiles[index].innerHTML = "☠";
             this.model.board[index] = 1;
             this.model.turnNum++
         } else if (this.model.turnNum % 2 == 1 && this.model.board[index] == 0) {
-            console.log("hellooooooo")
-            tiles[index].innerHTML = "o";
+            tiles[index].innerHTML = "☮";
             this.model.board[index] = -1;
             this.model.turnNum++
 
         }
+        if (this.model.turnNum > 4){
+            this.checkWin()
+        }
+    }
+
+    checkWin() {
+        for (let i = 0; i < this.model.winBoard.length; i++){
+            let sum = 0;
+            for (let j = 0; j < this.model.winBoard[i].length; j++) {
+                sum += this.model.board[this.model.winBoard[i][j]]
+                if (sum == 3) {
+                    this.winner("☠")
+                    return;
+                }
+                else if (sum == -3) {
+                    this.winner("☮")
+                    return
+                }
+            }
+        }
+        if (this.model.board.every(e => e != 0)) {
+            console.log("TIE")
+            this.winner("😼")
+        }
+    }
+
+    winner(marker) {
+        this.model.board.fill(2)
+        this.model.turnNum = 0;
+        this.view.elementMake("div", "text-center fs-1", "", "", "", this.view.app, `${marker} Wins!` )
+        this.view.elementMake("button", "", "", "", "app.view.init()", this.view.app, "Reset Game?" )
+        // let win = document.createElement("div");
+        // win.className = "text-center fs-1"
+        // win.innerHTML = marker + " wins! <br>"
+        // app.append(win)
+        // let restart = document.createElement("button");
+        // restart.innerHTML = "Reset Game?"
+        // restart.addEventListener("click", ()=> {
+        //     app.innerHTML = ""
+        //     who()
+        // })
+        // win.appendChild(restart)
+    }
+    restart() {
+        this.view.app.innerHTML = "";
+        
     }
 
 }
-
+//()=>{this.view.app.innerHTML = ''}
 // console.log("hello")
 const app = new gameController(new gameData(), new gameView())
 app.view.init()
